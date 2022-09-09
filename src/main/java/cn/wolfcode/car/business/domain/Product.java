206340d93a99b539e9e2a64f024c933c62fc90bc
小关 <18813450179@163.com>
2022-09-09 19:18:46 +0800

    添加一个产品的类

---
+++ src/main/java/cn/wolfcode/car/business/domain/Product.java
@@ -0,0 +1,12 @@
+package cn.wolfcode.car.business.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Product {
+    private String name;
+}
