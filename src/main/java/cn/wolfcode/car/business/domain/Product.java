bc231b9cf92c92dbfc15ab0eb4bc078cb923ca38
小关 <18813450179@163.com>
2022-09-09 19:20:30 +0800

    添加一个产品的类

--- src/main/java/cn/wolfcode/car/business/domain/Product.java
+++ src/main/java/cn/wolfcode/car/business/domain/Product.java
@@ -4,9 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.math.BigDecimal;
+
 @Setter
 @Getter
 @ToString
 public class Product {
     private String name;
+    private BigDecimal price;// 价格
 }
