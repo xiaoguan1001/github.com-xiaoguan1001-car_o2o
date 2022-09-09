4f13d2f067582666416dc68a4dd3eaa19a27d24c
小关 <18813450179@163.com>
2022-09-09 19:52:55 +0800

    模拟冲突2

--- src/main/java/cn/wolfcode/car/business/domain/Aaa.java
+++ src/main/java/cn/wolfcode/car/business/domain/Aaa.java
@@ -1,7 +1,12 @@
 package cn.wolfcode.car.business.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Setter
+@Getter
 public class Aaa {
     private String bbb;
     private BigDecimal ccc;
