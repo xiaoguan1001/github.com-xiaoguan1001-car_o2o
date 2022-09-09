dd55e079b7a8dcfcb16b31a8859dc5ce7aece89d
雪碧不加冰 <569933185@qq.com>
2022-09-09 18:46:33 +0800

    人员实体类

---
+++ src/main/java/cn/wolfcode/car/business/domain/People.java
@@ -0,0 +1,17 @@
+package cn.wolfcode.car.business.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class People {
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 性别
+     */
+    private String gender;
+}
